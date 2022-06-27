b59cb9f10692902e28e94fddb726d6a77ef0a664
kon010101 <kornisemail@gmail.com>
2022-06-27 19:14:09 +0200

    Fix type module

---
+++ scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# remove dist folder
+rm -rf dist/
+
+# build
+npm run build
+
+# remove dist from gitignore
+sed -i.bak '/^dist\//d' .gitignore
+
+# Stage all changes
+git add dist/
+
+# commit changes
+git commit -m "Deploy to subtree branch (gh-pages)"
+
+# deploy to subtree branch
+git subtree push --prefix dist origin gh-pages
+
+# undo last commit
+git reset --soft HEAD~
+
+# Unstage all changes
+git reset
+
+# Restore original .gitignore
+git restore .gitignore
