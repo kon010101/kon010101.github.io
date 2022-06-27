534699048a23c94a3516e1072066138a22669c2b
kon010101 <kornisemail@gmail.com>
2022-06-27 12:47:34 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const Accordion = require("./accordion");
+
+const accordion = new Accordion({
+  headerClass: ".Accordion__Item .Item__Header",
+  contentClass: ".Item__ContentContainer",
+});
+
+console.log(accordion);
