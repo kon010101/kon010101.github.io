e0c791556d0b95de58cff67c8feed1071b7a76d3
kon010101 <kornisemail@gmail.com>
2022-06-28 15:53:15 +0200

    Switch deployment method to gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,19 +6,7 @@ rm -rf dist/
 # build
 npm run build
 
-# Stage all changes
-git add dist -f
+# deploy to gh pages
+gh-pages -d dist
 
-# commit changes
-git commit -am "Deploy to subtree branch (gh-pages)"
-
-# deploy to subtree branch
-git subtree pull --prefix dist/ origin gh-pages
-git subtree push --prefix dist/ origin gh-pages
-
-# undo last commit
-git reset --soft HEAD~
-
-# Unstage all changes
-git reset
 
