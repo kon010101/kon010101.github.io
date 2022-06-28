dde3c0906808edef5e15e9e4157cada3cedbe2c7
kon010101 <kornisemail@gmail.com>
2022-06-28 14:21:50 +0200

    Deploy to subtree branch (gh-pages)

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const Accordion = require("./accordion");
-
-const accordion = new Accordion({
-  headerClass: ".Accordion__Item .Item__Header",
-  contentClass: ".Item__ContentContainer",
-});
-
-console.log(accordion);
